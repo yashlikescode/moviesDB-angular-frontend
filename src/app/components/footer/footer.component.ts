d8690fd38c8371ee4e2014f33f7df76504aa903e
stlyash <kmryashasvi@gmail.com>
2026-07-04 23:56:09 +0530

    added styling changes to header and home

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear: number = new Date().getFullYear();
+}
